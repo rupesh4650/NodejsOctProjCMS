6640106d467ffa996e41d6a2452d9b2d9ee2e540
rupesh4658 <rkyadav4650@gmail.com>
2023-10-06 10:49:44 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+//set view engins as ejs
+app.set("view engine","ejs")
+
+//form bata data aai rahya xa paese gae or handle gar
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//all blogs
+app.get("/",(req,res)=>{
+    res.render("blogs")
+})
+
+//create blog
+app.get("/createblog",(req,res)=>{
+    res.render("createblog")
+})
+
+//create blog post
+app.post("/createblog",(req,res)=>{
+    console.log(req.body)
+    res.send("Form submitted successfully")
+})
+
+
+app.listen(3000,(req,res)=>{
+    console.log("nodejs project has been running on port 3000")
+})
